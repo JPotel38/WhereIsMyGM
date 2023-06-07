b1334becd72d335c7e8d059e5804f75b9c50d497
Jérémy Potel <potel.jeremy@gmail.com>
2023-06-07 11:58:33 +0200

    style: rework Nav

--- src/front/src/components/layout/Nav.js
+++ src/front/src/components/layout/Nav.js
@@ -1,77 +1,56 @@
 import React, {useContext} from 'react';
-import {Button, Dropdown, Image, Layout, Menu, Typography} from 'antd';
+import {Button, Dropdown, Layout, Menu, Typography} from 'antd';
 import {Link} from 'react-router-dom';
-import '../../App.css';
+import {UserOutlined} from '@ant-design/icons';
+import {authContext} from '../../AuthContext';
 import './Nav.scss';
-import {authContext} from "../../AuthContext";
 
 const {Header} = Layout;
 const {Title} = Typography;
 
 function Nav() {
-    const items = [];
     const {auth, setAuthData} = useContext(authContext);
 
     const logout = () => {
         setAuthData(null);
-    }
+    };
 
-    if (auth.data.user) {
+    const renderDropdownMenu = () => {
+        return (<Menu>
+            <Menu.Item key="1"><Link to="/account">Account</Link></Menu.Item>
+            <Menu.Item key="2"> <Button onClick={logout}>Logout</Button></Menu.Item>
+        </Menu>);
+    };
 
-        const accountMenu = (
-            <Menu>
-                <Menu.Item key='0'>
-                    <Link to='/account'>Account</Link>
-                </Menu.Item>
-                <Menu.Item key='1'>
-                    <Button onClick={logout}>Logout</Button>
-                </Menu.Item>
-            </Menu>
-        );
-
-        let itemAccount =
-            <Dropdown overlay={accountMenu}
-                      trigger={['click']}
-            >
-                <a onClick={(e) => e.preventDefault()}>
-                    <Image className='accountProfilePicture'
-                           width={35}
-                           src={auth.data.user.profilePicture}
-                    />
-                </a>
-            </Dropdown>
-        items.push(itemAccount);
-    } else {
-        let itemConnexion =
-            <Menu.Item key="login">
-                <Link to='/login'>Log in</Link>
+    return (<Header>
+        <Menu mode="horizontal" style={{width: '100%'}}>
+            <Menu.Item key="0">
+                <Title>
+                    <Link to="/">WhereIsMyGM?</Link>
+                </Title>
             </Menu.Item>
-        let itemInscription =
-            <Menu.Item key="signup" style={{float: 'right'}}>
-                <Link to='/signup'>Sign up</Link>
+            <Menu.Item key="1">
+                <Link to="/gm">Game Masters</Link>
             </Menu.Item>
-
-        items.push(itemConnexion, itemInscription);
-    }
-
-    return (
-        <Header>
-            <Menu mode="horizontal" style={{width: "100%"}}>
-                <Menu.Item key="1">
-                    <Title>
-                        <Link to="/">WhereIsMyGM?</Link>
-                    </Title>
+            <Menu.Item key="2">
+                <Link to="/games">Games</Link>
+            </Menu.Item>
+            {auth.data ? (<Menu.Item key="3" className="account-dropdown" style={{float: 'right'}} >
+                <Dropdown overlay={renderDropdownMenu()} trigger={['click']}>
+              <span>
+                <UserOutlined/>
+              </span>
+                </Dropdown>
+            </Menu.Item>) : (<>
+                <Menu.Item key="4" className="nav-link" style={{float: 'right'}}>
+                    <Link to="/login">Log in</Link>
                 </Menu.Item>
-                <Menu.Item key="10">
-                    <Link to='/gm'>Game Masters</Link>
+                <Menu.Item key="5" className="nav-link" style={{float: 'right'}}>
+                    <Link to="/signup">Sign up</Link>
                 </Menu.Item>
-                <Menu.Item key="11">
-                    <Link to='/games'>Games</Link>
-                </Menu.Item>
-                {items}
-            </Menu>
-        </Header>
-    )
+            </>)}
+        </Menu>
+    </Header>);
 }
 
-export default Nav
+export default Nav;
