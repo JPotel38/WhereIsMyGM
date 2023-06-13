d6a9cbf173abde15fc6cf677b78be620e105713f
Jérémy Potel <potel.jeremy@gmail.com>
2023-06-13 11:21:51 +0200

    feat: add fetch departements API

--- src/routes/geolocalisation.js
+++ src/routes/geolocalisation.js
@@ -6,4 +6,8 @@ router.get('/regions', async function (req, res) {
    axios.get('https://geo.api.gouv.fr/regions').then(response => res.send(response.data));
 });
 
+router.get('/departements', async function (req, res) {
+   axios.get('https://geo.api.gouv.fr/departements').then(response => res.send(response.data));
+});
+
 module.exports = router;
