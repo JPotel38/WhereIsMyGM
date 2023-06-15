b2b33393c9670d5da1de3110aea0a8e35dd1e8af
Jérémy Potel <potel.jeremy@gmail.com>
2023-06-15 16:43:49 +0200

    feat: create GameMasterAccount and display games list

--- src/front/src/components/pages/GameMasterAccount.js
+++ src/front/src/components/pages/GameMasterAccount.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import '../../App.scss';
-import {Button} from "antd";
+import {Button, List} from "antd";
 import {Content} from "antd/es/layout/layout";
 import Title from "antd/es/typography/Title";
 import {Link} from "react-router-dom";
+import {authContext} from "../../AuthContext";
 
 function GameMasterAccount() {
+    const {auth, setAuthData} = useContext(authContext);
+    const [listGames, setListGames] = useState([])
 
-    return (
-        <Content style={{padding: '0 50px'}}>
-            <Title>Game Master Account</Title>
-            <Button><Link to="/signupGM">Modifier</Link></Button>
-        </Content>)
+    useEffect(() => {
+        const fetchGames = async () => {
+            const response = await fetch('/users/gamesbyuser/' + auth.data.user._id);
+            const bodyGames = await response.json();
+            setListGames(bodyGames.listGames)
+        };
+
+        fetchGames();
+    }, []);
+
+    return (<Content style={{padding: '0 50px'}}>
+        <Title>Game Master Account</Title>
+        <List
+            header={<div>Your Games</div>}
+            bordered
+            dataSource={listGames}
+            renderItem={(item) => (
+                <List.Item>
+                    {item.title} {item.edition}
+                </List.Item>
+            )}
+        />
+        <Button><Link to="/updategmaccount">Modifier</Link></Button>
+    </Content>)
 }
 
 export default GameMasterAccount
