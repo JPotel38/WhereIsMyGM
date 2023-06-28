3d0143444456d1e5f6e1b5292c08951cc99e5877
Jérémy Potel <potel.jeremy@gmail.com>
2023-06-28 16:07:15 +0200

    refactor: useGamesListHook in ts

--- src/front/src/hooks/useGamesListHook.ts
+++ src/front/src/hooks/useGamesListHook.ts
@@ -1,13 +1,14 @@
 import {useEffect, useState} from 'react';
+import {IGame} from "../interfaces/GameInterface";
 
-function useGamesList(url) {
+function useGamesList(url: string) {
     const [listGames, setListGames] = useState([]);
     useEffect(() => {
         const fetchGameList = async () => {
             try {
                 const response = await fetch(url);
                 const data = await response.json();
-                setListGames(data.sort((a, b) => {
+                setListGames(data.sort((a: IGame, b: IGame) => {
                     const x = a.title.toLowerCase();
                     const y = b.title.toLowerCase();
                     if (x < y) {
