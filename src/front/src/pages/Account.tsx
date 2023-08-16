00fed3f90d05879b8a6ade01c8d1b6d91a19a499
Jérémy Potel <potel.jeremy@gmail.com>
2023-08-16 10:14:48 +0200

    feat: User Account Beta

--- src/front/src/pages/Account.tsx
+++ src/front/src/pages/Account.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import '../App.scss';
 import Title from "antd/es/typography/Title";
 import {Content} from "antd/es/layout/layout";
-
+import {authContext} from "../AuthContext";
+import {IUser} from "../interfaces/UserInterface";
 
 function Account() {
+    const {auth, setAuthData} = useContext(authContext);
+    const [userData, setUserData] = useState<IUser>({
+        email: "",
+        userPseudo: "",
+        firstName: "",
+        lastName: "",
+        password: "",
+        profilePicture: "",
+        address: {
+            city: "",
+            departement: "",
+            postalCode: "",
+            region: "",
+            country: ""
+        },
+    });
+
+    useEffect(() => {
+        if (auth.data) {
+            setUserData(auth.data.user)
+        }
+    }, []);
 
     return (
-        <Content style={{padding: '0 50px'}}>
-            <Title>User Account</Title>
-        </Content>
+            <Content>
+                <Title>User Account</Title>
+            <ul>
+                <li>Pseudonyme : {userData.userPseudo}</li>
+                <li>Prénom : {userData.firstName}</li>
+                <li>Nom de famille : {userData.lastName}</li>
+                <li>Email : {userData.email}</li>
+            </ul>
+            </Content>
     )
 }
 
